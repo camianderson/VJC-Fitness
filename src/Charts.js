6b46f9b1fffdbac6462a68bd9425602d12b21ba6
Jordan Farelli <jordanfarelli@gmail.com>
2022-06-02 18:52:32 -0600

    created chart display functionality in charts.js file

--- src/Charts.js
+++ src/Charts.js
@@ -37,23 +37,83 @@ export function displayWeeklySleepChart(chart, date, sHours, sqHours) {
     var chart = new Chart("sleepChart", config)
 }
 
-
 export function displayWeeklyWaterChart(chart, date, ounces) {
-var barColors = ["steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue",];
-chart.data.labels = date;
-chart.data.datasets = [{
-    label: "Ounces Consumed Per Date",
-    fill: false,
-    backgroundColor: barColors,
-    data: ounces
-}]
-chart.options = {
-    legend: {
-        display: false
-    },
-    title: {
-        display: true
+    var barColors = ["steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue",];
+    chart.data.labels = date;
+    chart.data.datasets = [{
+        label: "Ounces Consumed Per Date",
+        fill: false,
+        backgroundColor: barColors,
+        data: ounces
+    }]
+    chart.options = {
+        legend: {
+            display: false
+        },
+        title: {
+            display: true
+        }
     }
+    chart.update()
 }
-chart.update()
+
+
+export function displayWeeklyStairsChart(chart, date, weeklyStairs) {
+    var barColors = ["steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue",];
+    chart.data.labels = date;
+    chart.data.datasets = [{
+        label: "Weekly Stairs Climbed",
+        fill: false,
+        backgroundColor: barColors,
+        data: weeklyStairs
+    }]
+    chart.options = {
+        legend: {
+            display: false
+        },
+        title: {
+            display: true
+        }
+    }
+    chart.update()
+}
+
+export function displayWeeklyStepsChart(chart, date, numSteps) {
+    var barColors = ["steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue",];
+    chart.data.labels = date;
+    chart.data.datasets = [{
+        label: "Weekly Steps Walked",
+        fill: false,
+        backgroundColor: barColors,
+        data: numSteps
+    }]
+    chart.options = {
+        legend: {
+            display: false
+        },
+        title: {
+            display: true
+        }
+    }
+    chart.update()
+}
+
+export function displayWeeklyMinutesActiveChart(chart, date, minutesActive) {
+    var barColors = ["steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue",];
+    chart.data.labels = date;
+    chart.data.datasets = [{
+        label: "Weekly Minutes Active",
+        fill: false,
+        backgroundColor: barColors,
+        data: minutesActive
+    }]
+    chart.options = {
+        legend: {
+            display: false
+        },
+        title: {
+            display: true
+        }
+    }
+    chart.update()
 }
