b82c9f3f015b95e4f1e5e9df84d5f5703c75307c
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-06-04 11:35:56 -0600

    removed unnecessary images and code

--- src/Charts.js
+++ src/Charts.js
@@ -1,44 +1,25 @@
-// 
-// export function displayWeeklySleepChart(chart, date, sHours, sqHours) {
-//     if(chart != null){
-//         chart.destroy()
-//     }
-//     var barColors = ['steelblue', 'steelblue', 'steelblue', 'steelblue', 'steelblue', 'steelblue', 'steelblue'];
-//     var barColors2 = ['cadetblue', 'cadetblue', 'cadetblue', 'cadetblue', 'cadetblue', 'cadetblue', 'cadetblue'];
-//     const data = {
-//         labels: date,
-//         datasets: [{
-//             label: 'Sleep Hours',
-//             backgroundColor: barColors,
-//             data: sHours
-//         },
-//         {
-//             label: 'Sleep Quality',
-//             backgroundColor: barColors2,
-//             data: sqHours
-//         }]
-//     }
-//     const config = {
-//         type: 'bar',
-//         data: data,
-//         options: {
-//             sqHours,
-//             sHours,
-//             legend: {
-//                 display: false
-//             },
-//             title: {
-//                 display: true,
-//                 text: 'Sleep Hours & Sleep Quality Per Day'
-//             }
-//         }
-//     }
-//
-//     var chart = new Chart("sleepChart", config)
-// }
+export function displayWeeklyWaterChart(chart, date, ounces) {
+    var barColors = "steelblue";
+    chart.data.labels = date;
+    chart.data.datasets = [{
+        label: "Ounces Consumed Per Date",
+        fill: false,
+        backgroundColor: barColors,
+        data: ounces
+    }]
+    chart.options = {
+        legend: {
+            display: false
+        },
+        title: {
+            display: true
+        }
+    }
+    chart.update()
+}
 
 export function displayWeeklySleepHoursChart(chart, date, sHours) {
-    var barColors = ["steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue",];
+    var barColors = "steelblue";
     chart.data.labels = date;
     chart.data.datasets = [{
         label: 'Hours of Sleep Per Day',
@@ -58,7 +39,7 @@ export function displayWeeklySleepHoursChart(chart, date, sHours) {
 }
 
 export function displayWeeklySleepQualityChart(chart, date, sqHours) {
-    var barColors = ["steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue",];
+    var barColors = 'cadetblue';
     chart.data.labels = date;
     chart.data.datasets = [{
         label: 'Hours of Quality Sleep Per Day',
@@ -77,29 +58,8 @@ export function displayWeeklySleepQualityChart(chart, date, sqHours) {
     chart.update()
 }
 
-export function displayWeeklyWaterChart(chart, date, ounces) {
-    var barColors = ["steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue",];
-    chart.data.labels = date;
-    chart.data.datasets = [{
-        label: "Ounces Consumed Per Date",
-        fill: false,
-        backgroundColor: barColors,
-        data: ounces
-    }]
-    chart.options = {
-        legend: {
-            display: false
-        },
-        title: {
-            display: true
-        }
-    }
-    chart.update()
-}
-
-
 export function displayWeeklyStairsChart(chart, date, weeklyStairs) {
-    var barColors = ["steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue", "steelblue",];
+    var barColors = "steelblue";
     chart.data.labels = date;
     chart.data.datasets = [{
         label: "Weekly Stairs Climbed",
@@ -119,7 +79,7 @@ export function displayWeeklyStairsChart(chart, date, weeklyStairs) {
 }
 
 export function displayWeeklyStepsChart(chart, date, numSteps) {
-    var barColors = ["cadetblue", "cadetblue", "cadetblue", "cadetblue", "cadetblue", "cadetblue", "cadetblue",];
+    var barColors = "cadetblue";
     chart.data.labels = date;
     chart.data.datasets = [{
         label: "Weekly Steps Walked",
@@ -139,7 +99,7 @@ export function displayWeeklyStepsChart(chart, date, numSteps) {
 }
 
 export function displayWeeklyMinutesActiveChart(chart, date, minutesActive) {
-    var barColors = ["LightSeaGreen", "LightSeaGreen", "LightSeaGreen", "LightSeaGreen", "LightSeaGreen", "LightSeaGreen", "LightSeaGreen",];
+    var barColors = "LightSeaGreen";
     chart.data.labels = date;
     chart.data.datasets = [{
         label: "Weekly Minutes Active",
